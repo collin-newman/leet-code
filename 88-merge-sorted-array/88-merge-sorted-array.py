ec0e97f32b1a3f15a574321226571bd091af16d1
Collin Newman <collin.newman37@gmail.com>
2022-03-03 11:05:46 -0500

    Time: 66 ms (23.99%), Space: 13.9 MB (93.68%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if len(nums1) == 0:
+            for num in nums2:
+                print(num)
+                nums1.append(num)
+                num1.pop()
+            return
+        
+        if len(nums1) == 1 and nums1[0] == 0:
+            for num in nums2:
+                nums1.append(num)
+                nums1.pop(0)
+            return
+        
+        
+        mergeArray = nums1
+        insertIndex = 0
+        inserts = 0
+        for _ in range(len(nums2)):
+            numToInsert = nums2[0]
+            for index in range(insertIndex, (m + n)):
+                if (index) < (m + n):
+                    if numToInsert <= mergeArray[index] or (mergeArray[index] == 0 and index >= (m + inserts)):
+                        insertIndex = index
+                        mergeArray.insert(insertIndex, numToInsert)
+                        nums2.pop(0)
+                        mergeArray.pop(len(mergeArray) - 1)
+                        inserts += 1
+                        break
+                    else:
+                        continue
+                else:
+                    insertIndex = len(mergeArray) - 1
+                    mergeArray.insert(numToInsert, insertIndex)
+                    nums2.pop(0)
+                    mergeArray.pop(len(mergeArray) - 1)
+                    break
+            
